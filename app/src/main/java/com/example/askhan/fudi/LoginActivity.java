7727a0c817ab5de0df59ebcb7d22874496a852a2
asif <=asifkhan61911@gmail.com>
2018-09-25 19:25:09 +0500

    drawer

--- app/src/main/java/com/example/askhan/fudi/LoginActivity.java
+++ app/src/main/java/com/example/askhan/fudi/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.askhan.fudi;
 
+        import android.content.Intent;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
         import android.view.WindowManager;
+        import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button btn = (Button) findViewById(R.id.logBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/askhan/fudi/MainActivity.java
+++ app/src/main/java/com/example/askhan/fudi/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.askhan.fudi;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageView imageView = (ImageView) findViewById(R.id.profile_photo);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.photo);
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
+
+
     }
 }
